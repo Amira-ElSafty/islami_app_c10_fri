1c03747626c66e99266fc3c909c9a3f256d4346a
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2024-02-02 11:41:00 +0200

    finshed quran tab. finshed hadeth tab.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_islami_c10_fri/home/hadeth/hadeth_details_screen.dart';
 import 'package:flutter_app_islami_c10_fri/home/home_screen.dart';
+import 'package:flutter_app_islami_c10_fri/home/quran/sura_details_screen.dart';
+import 'package:flutter_app_islami_c10_fri/my_theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +16,10 @@ class MyApp extends StatelessWidget {
       initialRoute: HomeScreen.routeName,
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
+        SuraDetaillsScreen.routeName: (context) => SuraDetaillsScreen(),
+        HadethDetaillsScreen.routeName: (context) => HadethDetaillsScreen(),
       },
+      theme: MyTheme.lightTheme,
     );
   }
 }
